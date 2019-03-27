ad29349df240e062aac8795789a0cdc9139349de
Dereck San Miguel <derecksanmiguel@dhcp-v074-170.mobile.uci.edu>
2019-03-26 19:41:35 -0700

    mostly completed, but still some bugs to fix

---
+++ assets/app.js
@@ -0,0 +1,60 @@
+//variable to swtich between animated and stil gif & to place my new gifs
+let toggle = false
+let animalList = []
+
+// lets you grab data value of selected buttons on HTML
+document.addEventListener('click', event => {
+    if (event.target.className === 'gifImages') {
+        let { animal } = event.target.dataset
+
+        fetch(`http://api.giphy.com/v1/gifs/search?&api_key=dc6zaTOxFJmzC&q=${animal}&limit=10&rating=pg`
+        )
+            // This is pulling the path to the URL and adding it to our HTML
+            .then(r => r.json())
+            .then(({ data }) => {
+                   for (let i = 0; i < data.length; i++) {
+                    let animated = data[i].images.fixed_height.url
+                    let still = data[i].images.fixed_height_still.url
+                    let temp = document.createElement('img')
+                    temp.setAttribute('id','gifImg')
+                    temp.setAttribute('src',still)
+                    temp.setAttribute('alt',animal)
+                    temp.setAttribute('data-still',still)
+                    temp.setAttribute('data-animated',animated)
+                    document.querySelector('#gifDiv').append(temp) 
+                }
+            })
+            .catch(event => console.error)
+            document.querySelector('#gifDiv').innerHTML = ''
+        //this allows me to play and pause the gif 
+    } else if (event.target.id === 'gifImg') {
+        let { animated, still } = event.target.dataset
+        toggle = !toggle
+        event.target.setAttribute('src', toggle ? animated : still)
+    }
+})
+
+// this is displaying what is being entered into the search box
+function renderAnimals() {
+    let autoAnimal = document.querySelector('#addAnimal').value
+
+    document.querySelector('#aniList').innerHTML = ''
+    animalList.forEach(item => {
+        let aniElem = document.createElement('button')
+        aniElem.setAttribute('class', 'gifImages')
+        aniElem.setAttribute('data-animal', autoAnimal)
+        aniElem.textContent = item
+        document.querySelector('#aniList').append(aniElem)
+    })
+}
+
+//this is pulling the value being entered the search box
+document.querySelector('#sbmtAnimal').addEventListener('click', e => {
+    e.preventDefault()
+    animalList.push(document.querySelector('#addAnimal').value)
+    document.querySelector('#addAnimal').value
+    renderAnimals()
+    document.querySelector('#addAnimal').value = ''
+
+})
+renderAnimals()
